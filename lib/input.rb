ccdeec7bae1c4ea84ecadefbc678f20d56d52edf
sevensidedmarble <andrewianstewart@gmail.com>
2017-09-18 15:47:31 -0400

    More basic structuring.

---
+++ lib/input.rb
@@ -0,0 +1,7 @@
+# The main input controller for the game. All input should be taken in here, and anything that needs input should call
+# these methods on the main Input instance of the Game object.
+class Input
+  def getting_input
+    gets
+  end
+end
---
+++ lib/player_controller.rb
@@ -0,0 +1,8 @@
+# Player_Controller is the interface by which the player controls a Hero. It is written such that any Actor could be given
+# a Player_Controller, and the player would then be able to move them during their turn, allowing behaviour such as Mind
+# Control spells.
+class Player_Controller
+  def update
+
+  end
+end
