63fc233763538afb3e26b83c6e8b28b0ae86ad11
sevensidedmarble <andrewianstewart@gmail.com>
2017-09-18 15:52:02 -0400

    Simple input

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,20 @@
+require 'input'
+
 class Game
   # the main game instance.
 
   def initialize
-    @turn_count = 1
+    @input = Input.new
+    @turn_count = 0
     @over = false
     @main_character = Hero.new(self)
     start_game
   end
 
   def update
+    @turn_count += 1
     puts "Beginning turn #{@turn_count}."
+    @input.getting_input
     @main_character.hit(2)
   end
 
--- lib/input.rb
+++ lib/input.rb
@@ -1,7 +1,9 @@
 # The main input controller for the game. All input should be taken in here, and anything that needs input should call
 # these methods on the main Input instance of the Game object.
 class Input
+  attr_accessor :pressed
+
   def getting_input
-    gets
+    @pressed = gets
   end
 end
